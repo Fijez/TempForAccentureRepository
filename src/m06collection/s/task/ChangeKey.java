4ade3e1b682df382c8ae7da584e3384d5dd0d7f3
Fijez <tem.est@mail.ru>
2021-10-15 22:32:04 +0300

    ChangeKey done

--- src/m06collection/s/task/ChangeKey.java
+++ src/m06collection/s/task/ChangeKey.java
@@ -1,19 +1,30 @@
 package m06collection.s.task;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Реализуйте метод, который получает на вход Map<M,S> и возвращает Map
  * ключи и значение должны поменяться местами
  */
 
-public class ChangeKey <T1,T2>{
+public class ChangeKey<T1, T2> {
     public static void main(String[] args) {
-
+        Map<String,Integer> map = new HashMap<>();
+        map.put("zero",1);
+        map.put("one",2);
+        map.put("two",3);
+        map.put("three",4);
+        map.put("fore",5);
+        ChangeKey ck = new ChangeKey();
+        map = ck.swap(map);
+        System.out.println(map.toString());
     }
-    public void swap(Map<T1, T2> map) {
-        for (int i = 0; i < map.size(); i++) {
+    public Map swap(Map<T1, T2> map) {
+        Map<T2, T1> newMap = new HashMap<>();
+        for (Map.Entry<T1, T2> entry:map.entrySet()) {
+            newMap.put(entry.getValue(), entry.getKey());
         }
+        return (Map<T1, T2>) newMap;
     }
 }
