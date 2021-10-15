ab0eb76a276b307dbd461a0413a7ababa2403c7e
Fijez <tem.est@mail.ru>
2021-10-15 10:54:31 +0300

    сделал ArrayContraLinked

--- src/m06collection/s/task/ArrayContraLinked.java
+++ src/m06collection/s/task/ArrayContraLinked.java
@@ -1,5 +1,9 @@
 package m06collection.s.task;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Реализуйте метод, который добавит 500000 элементов в ArrayList и LinkedList.
  * реализуйте метод, который выберет из заполненного списка наугад элементы 500000 раз.
@@ -9,5 +13,51 @@ package m06collection.s.task;
  *
  */
 public class ArrayContraLinked {
-    //TODO
+    public static void main(String[] args) {
+        double time1;
+        double time2;
+        List <Integer> arrayList = new ArrayList<>();
+        List <Integer> linkedList = new LinkedList<>();
+        time1 = System.currentTimeMillis();
+        addFiftyThousands(arrayList);
+        time1 =  System.currentTimeMillis() - time1;
+        time2 = System.currentTimeMillis();
+        outputFiftyThousands(arrayList);
+        time2 = System.currentTimeMillis() - time2;
+
+        time1 /= 1000;
+        time2 /= 1000;
+        double time = time1 + time2;
+        System.out.println();
+        System.out.println(time1 + " " + time2 + " " + time);//0.035 0.051 0.086
+
+        System.out.println("------------------------------------------------------");
+
+        time1 = System.currentTimeMillis();
+        addFiftyThousands(linkedList);
+        time1 =  System.currentTimeMillis() - time1;
+        time2 = System.currentTimeMillis();
+        outputFiftyThousands(linkedList);
+        time2 = System.currentTimeMillis() - time2;
+        time1 /= 1000;
+        time2 /= 1000;
+        time = time1 + time2;
+        System.out.println();
+        System.out.println(time1 + " " + time2 + " " + time);//0.156 486.959 487.115
+
+    }
+
+    public static void addFiftyThousands(List <Integer> list) {
+        for (int i = 0; i < 500000; i++) {
+            list.add((int) (Math.random()*100000));
+        }
+    }
+
+    public static void outputFiftyThousands(List <Integer> list) {
+        for (int i : list) {
+            int j = list.get((int) (Math.random() * 500000));
+        }
+    }
+
+
 }
--- src/m06collection/s/task/ChangeKey.java
+++ src/m06collection/s/task/ChangeKey.java
@@ -1,10 +1,19 @@
 package m06collection.s.task;
 
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Реализуйте метод, который получает на вход Map<M,S> и возвращает Map
  * ключи и значение должны поменяться местами
  */
 
-public class ChangeKey {
-    //TODO
+public class ChangeKey <T1,T2>{
+    public static void main(String[] args) {
+
+    }
+    public void swap(Map<T1, T2> map) {
+        for (int i = 0; i < map.size(); i++) {
+        }
+    }
 }
