7796bddab6f684fdc58f5104c18a7fa98f76052f
Fijez <tem.est@mail.ru>
2021-10-16 23:54:56 +0300

    ChangeKey done GenericSample done PerfomanceAcademic done SearchAndDelete done

--- src/m06collection/s/task/StringsLinkedLis.java
+++ src/m06collection/s/task/StringsLinkedLis.java
@@ -11,6 +11,122 @@ package m06collection.s.task;
  *
  */
 
-public class StringsLinkedLis {
-    //TODO
+class Demo {
+    public static void main(String[] args) {
+        StringsLinkedLis str = new StringsLinkedLis();
+        str.add("1");
+        str.add("2");
+        str.add("3");
+        str.add("4");
+        str.add("5");
+        str.add("6");
+        str.add("7");
+        str.add("8");
+        str.add("9");
+
+        StringsLinkedLis.node nd = str.getFirst();
+        System.out.println(nd.getValue());
+        nd.getNext();
+        System.out.println(nd.getValue());
+
+        nd.getNext();
+        System.out.println(nd.getValue());
+
+        nd.getNext();
+        System.out.println(nd.getValue());
+
+        nd.getNext();
+        System.out.println(nd.getValue());
+
+        nd.getNext();
+        System.out.println(nd.getValue());
+
+        nd.getNext();
+        System.out.println(nd.getValue());
+
+        System.out.println("-----------------------------------");
+
+        System.out.println(nd.getValue());
+        nd.getPrev();
+        System.out.println(nd.getValue());
+        nd.getPrev();
+        System.out.println(nd.getValue());
+        nd.getPrev();
+        System.out.println(nd.getValue());
+        nd.getPrev();
+        System.out.println(nd.getValue());
+        nd.getPrev();
+        System.out.println(nd.getValue());
+    }
+
 }
+
+
+class StringsLinkedLis {
+
+
+    private node first;
+    private node last;
+
+    public void add(String str) {
+        if (this.first == null) {
+            this.first = this.last = new node(str);
+        } else if (this.first == this.last){
+            this.last = new node(str);
+            this.last.prev = this.first;
+            this.first.next = last;
+        } else {
+            this.last.prev.next = new node(str);
+            this.last = this.last.prev.next;
+        }
+    }
+
+    public StringsLinkedLis() {
+    }
+
+    public node getFirst() {
+        return first;
+    }
+
+    public node getLast() {
+        return last;
+    }
+
+    class node {
+        private node next = null;
+        private node prev = null;
+        private String value = null;
+
+        public void getNext() {
+            if(next != null) {
+                prev = this;
+                next = next.next;
+                value = prev.value;
+            }
+        }
+
+        public void getPrev() {
+            if (prev != null) {
+                next = this;
+                prev = prev.prev;
+                value = next.value;
+            }
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public node (String val) {
+            value = val;
+        }
+
+        public node () {
+        }
+
+    }
+
+
+}
+
+
