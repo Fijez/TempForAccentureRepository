26227fa7301f042fcac619088a63d0f41a1cacd0
Fijez <tem.est@mail.ru>
2021-10-16 21:24:07 +0300

    ChangeKey done GenericSample done PerfomanceAcademic done SearchAndDelete done

--- src/m06collection/s/task/SearchAndDelete.java
+++ src/m06collection/s/task/SearchAndDelete.java
@@ -1,6 +1,11 @@
 package m06collection.s.task;
 
 
+import javax.swing.text.StyledEditorKit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 /**
  * В классе SearchAndDelete реализуйте следующие методы:
  * - метод removeBugWithFor(ArrayList<String>) , который удаляет строку из списка,
@@ -17,5 +22,56 @@ package m06collection.s.task;
  *  список слов : Risha, Easy ug,bug, Bug, Kerry,Elly, Amigo,Hello world!, I'd like Java
  */
 public class SearchAndDelete {
-    //TODO
+
+    public static void main(String[] args) {
+
+       ArrayList<String> arr = new ArrayList <>();
+       arr.addAll(Arrays.asList(
+               "Risha",
+               "Easy",
+               "ug" ,
+               "bug",
+               "bUg",
+               "Bug",
+               "Kerry",
+               "Elly",
+               "Amigo",
+               "Hello world!",
+               "I'd like Java"));
+        removeBugWithCopy(arr);
+       arr.stream().forEach(System.out::println);
+    }
+
+    public static void removeBugWithCopy(ArrayList<String> arr) {
+        ArrayList<String> copy = new ArrayList<>(arr);
+
+        for (String str : arr) {
+            if (str.equalsIgnoreCase("bug")) {
+                copy.remove(str);
+            }
+        }
+        arr.clear();
+        arr.addAll(copy);
+    }
+
+    public static void removeBugWithWhile(ArrayList<String> arr) {
+        Iterator<String> iter = arr.iterator();
+
+        while(iter.hasNext()) {
+            if (iter.next().equalsIgnoreCase("bug")){
+                iter.remove();
+            }
+        }
+    }
+
+public static void removeBugWithFor(ArrayList<String> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        if(arr.get(i).equalsIgnoreCase("bug")){
+            arr.remove(i);
+            i--;
+        }
+
+    }
+}
+
 }
