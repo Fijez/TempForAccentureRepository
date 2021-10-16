d3c3ff8eff990c9e065247601742bd4255ef3ec5
Fijez <tem.est@mail.ru>
2021-10-16 18:56:51 +0300

    ChangeKey done GenericSample done PerfomanceAcademic done

--- src/m06collection/s/task/PerfomanceAcademic.java
+++ src/m06collection/s/task/PerfomanceAcademic.java
@@ -1,5 +1,7 @@
 package m06collection.s.task;
 
+import java.util.*;
+
 /**
  * В классе PerfomanceAcademic необходимо объявить поле grades ( типа HashMap<String, Double>)
  * ключ- имя и фамилия студента, значение - средняя оценка.
@@ -10,6 +12,54 @@ package m06collection.s.task;
  */
 
 public class PerfomanceAcademic {
+    public PerfomanceAcademic(Map<String, Double> grades) {
+        this.grades = grades;
+    }
 
-    //TODO
+    public static void main(String[] args) {
+        Map<String, Double> map = new HashMap<>();
+        map.put("A a",0.0);
+        map.put("B b",1.0);
+        map.put("C c",3.0);
+        map.put("D d",2.0);
+        map.put("E e",10.0);
+        map.put("F f",5.0);
+        PerfomanceAcademic perfomanceAcademic = new PerfomanceAcademic(map);
+        perfomanceAcademic.sortGrades();
+        System.out.println(perfomanceAcademic.getAverageScore());
+        perfomanceAcademic.outputNames();
+        Double[] arr = perfomanceAcademic.getAverageScoreOfEach();
+        System.out.println(Arrays.toString(arr));
+    }
+
+    Map<String, Double> grades = new HashMap<>();
+
+    public void outputNames () {
+        for (String str : grades.keySet()) {
+            System.out.println(str.split(" ")[0]);
+        }
+    }
+
+    public Double[] getAverageScoreOfEach() {
+        Collection<Double> values = grades.values();
+        Double[] arr = values.toArray(new Double[0]);
+        return arr;
+    }
+
+    public Double getAverageScore() {
+       double avSc = 0;
+        for (Double score : grades.values()) {
+            avSc += score;
+        }
+        return avSc/grades.size();
+
+    }
+
+    public void sortGrades() {
+        grades.entrySet().
+                stream().
+                sorted(Map.Entry.<String, Double>comparingByValue().reversed()).
+                forEach(System.out::println);
+    }
+
 }
