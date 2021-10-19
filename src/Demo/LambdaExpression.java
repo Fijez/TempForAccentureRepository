45c2b8d5be8818e08b85765d1de0cdc776326fa2
Fijez <tem.est@mail.ru>
2021-10-19 09:29:14 +0300

    анятие 18.10.2021

---
+++ src/Demo/LambdaExpression.java
@@ -0,0 +1,44 @@
+package Demo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ *  написать метод:
+ *  -вычисления максимального нечетного числа
+ *  в диапазоне от 5 до 13 и вернуть его квадрат
+ *
+ *  -метод который получает два числа введенных с клавиатуры  и возвращает их произведение
+ *
+ *  - метод который принимает  три числа ( a, b,c )- стороны треугольника
+ *  и возвращает площадь треугольника
+ *
+ */
+
+public class LambdaExpression {
+
+    public static void main(String[] args) {
+        Function() mySort =
+    }
+
+    public Double maxOdd () {
+        Double[] numb = {5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0};
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(int o1, int o2) {
+                if (o1 % 2 != 0 && o2 % 2 != 0) {
+                    return (o1 > o2? o1:o2);
+                } else if (o1 % 2 != 0) {
+
+                } else if(o1 % 2 != 0) {
+
+                } else {
+
+                }
+                return 0;
+            }
+        }
+        Arrays.stream(numb).max(comparator);
+        return 0.0;
+    }
+}
