d438dc7a02d068c0f9635ecbe318f757a0dad2c5
Fijez <tem.est@mail.ru>
2021-10-24 11:08:42 +0300

    m08 in process FileOwnFileTwo (?) WindowAndLinux (difference?)

--- src/m08io/task/FileOwnFileTwo.java
+++ src/m08io/task/FileOwnFileTwo.java
@@ -1,5 +1,8 @@
 package m08io.task;
 
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * Напишите программу , которая будет считыватьс консоли путь к файлу1 и файлу2
  * Далее все байты из файла 1 записывает в файл2 и меняте их местами:
@@ -10,5 +13,23 @@ package m08io.task;
 //условие?
 
 public class FileOwnFileTwo {
-    //TODO
+    public static void main(String[] args) {
+        System.out.println("Введите полный путь к файлу 1:");
+        File file1 = new File(readPath());
+        System.out.println("Введите полный путь к файлу 2:");
+        File file2 = new File(readPath());
+
+
+    }
+
+    public static String readPath() {
+        Scanner in = new Scanner (System.in);
+        return in.nextLine();
+    }
+
+    public static void recordingBytes (File file1, File file2) throws IOException {
+        byte[] temp = new byte[(int) (file1.length() + file2.length())];
+        FileOutputStream fileOutputStream = new FileOutputStream(file2);
+        fileOutputStream.write(new FileInputStream(file1).readAllBytes());
+    }
 }
--- src/m08io/task/WindowAndLinux.java
+++ src/m08io/task/WindowAndLinux.java
@@ -1,10 +1,25 @@
 package m08io.task;
 
+import java.io.*;
+
 /**
  * Реализуйте программу в которой производиться чтения из файла тремя способами:
  * отдельно для windows linux и для любой ОС
  */
 
 public class WindowAndLinux {
-    //TODO
+    public static byte[] windowsRead(File file) throws IOException {
+        FileInputStream input = new FileInputStream(file);
+        return input.readAllBytes();
+    }
+
+    public static byte[] linuxRead(File file) throws IOException {
+        FileInputStream input = new FileInputStream(file);
+        return input.readAllBytes();
+    }
+
+    public static byte[] universalRead(File file) throws IOException {
+        FileInputStream input = new FileInputStream(file);
+        return input.readAllBytes();
+    }
 }
